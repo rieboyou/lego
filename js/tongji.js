0a3ca4073cba2c2fd6aff9f51f67724ad9c6789e
Heidy <rieboyou@gmail.com>
2025-03-27 02:05:39 +0800

    Add files via upload

---
+++ js/tongji.js
@@ -0,0 +1,8 @@
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fdd22d638e12ad75966ef313d40e9fa1";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
