fbc3eb3cf424934c4c579df2d5f2b99cbbac5e50
Heidy <rieboyou@gmail.com>
2025-03-27 16:39:24 +0800

    Add files via upload

--- lego/js/bricklink-colors.js
+++ lego/js/bricklink-colors.js
@@ -1,4 +1,6 @@
 let ALL_BRICKLINK_SOLID_COLORS = [
+    /**不顯示的顏色註解掉 */
+    /*
     {
         name: "White",
         hex: "#ffffff",
@@ -447,6 +449,395 @@ let ALL_BRICKLINK_SOLID_COLORS = [
         name: "Medium Tan",
         hex: "#D9C594",
         id: 241,
+    }, 
+    */{
+        name: "test1",
+        hex: "#f7f0e3",
+        id: 500,
+    }, {
+        name: "test2",
+        hex: "#a3a9aa",
+        id: 501,
+    }, {
+        name: "test3",
+        hex: "#6f7176",
+        id: 502,
+    }, {
+        name: "test4",
+        hex: "#4b4c50",
+        id: 503,
+    }, {
+        name: "test5",
+        hex: "#302e32",
+        id: 504,
+    }, {
+        name: "test6",
+        hex: "#523e37",
+        id: 505,
+    }, {
+        name: "test7",
+        hex: "#73483a",
+        id: 506,
+    }, {
+        name: "test8",
+        hex: "#ab5827",
+        id: 507,
+    }, {
+        name: "test9",
+        hex: "#b37c51",
+        id: 508,
+    }, {
+        name: "test10",
+        hex: "#de9064",
+        id: 509,
+    }, {
+        name: "test11",
+        hex: "#eca279",
+        id: 510,
+    }, {
+        name: "test12",
+        hex: "#f1b993",
+        id: 511,
+    }, {
+        name: "test13",
+        hex: "#ebc0a4",
+        id: 512,
+    }, {
+        name: "test14",
+        hex: "#ffd7c3",
+        id: 513,
+    }, {
+        name: "test15",
+        hex: "#fdede7",
+        id: 514,
+    }, {
+        name: "test16",
+        hex: "#dfc790",
+        id: 515,
+    }, {
+        name: "test17",
+        hex: "#998769",
+        id: 516,
+    }, {
+        name: "test18",
+        hex: "#ff7e30",
+        id: 517,
+    }, {
+        name: "test19",
+        hex: "#ffae11",
+        id: 518,
+    }, {
+        name: "test20",
+        hex: "#ffd300",
+        id: 519,
+    }, {
+        name: "test21",
+        hex: "#f1c277",
+        id: 520,
+    }, {
+        name: "test22",
+        hex: "#fff076",
+        id: 521,
+    }, {
+        name: "test23",
+        hex: "#cbf193",
+        id: 522,
+    }, {
+        name: "test24",
+        hex: "#9cbb27",
+        id: 523,
+    }, {
+        name: "test25",
+        hex: "#00a64d",
+        id: 524,
+    }, {
+        name: "test26",
+        hex: "#00863a",
+        id: 525,
+    }, {
+        name: "test27",
+        hex: "#1a4d3b",
+        id: 526,
+    }, {
+        name: "test28",
+        hex: "#8d8f63",
+        id: 527,
+    }, {
+        name: "test29",
+        hex: "#749883",
+        id: 528,
+    }, {
+        name: "test30",
+        hex: "#c1efe3",
+        id: 529,
+    }, {
+        name: "test31",
+        hex: "#00b5cc",
+        id: 530,
+    }, {
+        name: "test32",
+        hex: "#93bce6",
+        id: 531,
+    }, {
+        name: "test33",
+        hex: "#58a3da",
+        id: 532,
+    }, {
+        name: "test34",
+        hex: "#0094d0",
+        id: 533,
+    }, {
+        name: "test35",
+        hex: "#0065b2",
+        id: 534,
+    }, {
+        name: "test36",
+        hex: "#23415d",
+        id: 535,
+    }, {
+        name: "test37",
+        hex: "#7b91a5",
+        id: 536,
+    }, {
+        name: "test38",
+        hex: "#673e87",
+        id: 537,
+    }, {
+        name: "test39",
+        hex: "#8154ac",
+        id: 538,
+    }, {
+        name: "test40",
+        hex: "#ae78c2",
+        id: 539,
+    }, {
+        name: "test41",
+        hex: "#c0aedf",
+        id: 540,
+    }, {
+        name: "test42",
+        hex: "#edaad6",
+        id: 541,
+    }, {
+        name: "test43",
+        hex: "#d85fa5",
+        id: 542,
+    }, {
+        name: "test44",
+        hex: "#b72c75",
+        id: 543,
+    }, {
+        name: "test45",
+        hex: "#cd2928",
+        id: 544,
+    }, {
+        name: "test46",
+        hex: "#853433",
+        id: 545,
+    }, {
+        name: "test47",
+        hex: "#b4823e",
+        id: 546,
+    }, {
+        name: "test48",
+        hex: "#484649",
+        id: 547,
+    }, {
+        name: "test49",
+        hex: "#7e7a85",
+        id: 548,
+    }, {
+        name: "test50",
+        hex: "#bfc3c9",
+        id: 549,
+    }, {
+        name: "test51",
+        hex: "#959ba3",
+        id: 550,
+    }, {
+        name: "test52",
+        hex: "#f79570",
+        id: 551,
+    }, {
+        name: "test53",
+        hex: "#ff7680",
+        id: 552,
+    }, {
+        name: "test54",
+        hex: "#009790",
+        id: 553,
+    }, {
+        name: "test55",
+        hex: "#dfc8df",
+        id: 554,
+    }, {
+        name: "test56",
+        hex: "#ca9fb1",
+        id: 555,
+    }, {
+        name: "test57",
+        hex: "#c081a1",
+        id: 556,
+    }, {
+        name: "test58",
+        hex: "#e27ba2",
+        id: 557,
+    }, {
+        name: "test59",
+        hex: "#c55285",
+        id: 558,
+    }, {
+        name: "test60",
+        hex: "#e054a1",
+        id: 559,
+    }, {
+        name: "test61",
+        hex: "#e14585",
+        id: 560,
+    }, {
+        name: "test62",
+        hex: "#c2535c",
+        id: 561,
+    }, {
+        name: "test63",
+        hex: "#e18b80",
+        id: 562,
+    }, {
+        name: "test64",
+        hex: "#9899b3",
+        id: 563,
+    }, {
+        name: "test65",
+        hex: "#8b76a6",
+        id: 564,
+    }, {
+        name: "test66",
+        hex: "#7474ba",
+        id: 565,
+    }, {
+        name: "test67",
+        hex: "#a73aa2",
+        id: 566,
+    }, {
+        name: "test68",
+        hex: "#724a5f",
+        id: 567,
+    }, {
+        name: "test69",
+        hex: "#724345",
+        id: 568,
+    }, {
+        name: "test70",
+        hex: "#aa8b6e",
+        id: 569,
+    }, {
+        name: "test71",
+        hex: "#ecd7d5",
+        id: 570,
+    }, {
+        name: "test72",
+        hex: "#ed502a",
+        id: 571,
+    }, {
+        name: "test73",
+        hex: "#f2962f",
+        id: 572,
+    }, {
+        name: "test74",
+        hex: "#fff999",
+        id: 573,
+    }, {
+        name: "test75",
+        hex: "#e7b067",
+        id: 574,
+    }, {
+        name: "test76",
+        hex: "#c0bba2",
+        id: 575,
+    }, {
+        name: "test77",
+        hex: "#b9a16d",
+        id: 576,
+    }, {
+        name: "test78",
+        hex: "#ffd4b9",
+        id: 577,
+    }, {
+        name: "test79",
+        hex: "#26828d",
+        id: 578,
+    }, {
+        name: "test80",
+        hex: "#1e4b70",
+        id: 579,
+    }, {
+        name: "test81",
+        hex: "#2675a5",
+        id: 580,
+    }, {
+        name: "test82",
+        hex: "#00b4d1",
+        id: 581,
+    }, {
+        name: "test83",
+        hex: "#5482be",
+        id: 582,
+    }, {
+        name: "test84",
+        hex: "#38453a",
+        id: 583,
+    }, {
+        name: "test85",
+        hex: "#445c37",
+        id: 584,
+    }, {
+        name: "test86",
+        hex: "#99a444",
+        id: 585,
+    }, {
+        name: "test87",
+        hex: "#56852f",
+        id: 586,
+    }, {
+        name: "test88",
+        hex: "#70ad92",
+        id: 587,
+    }, {
+        name: "test89",
+        hex: "#97d4b7",
+        id: 588,
+    }, {
+        name: "test90",
+        hex: "#77adb2",
+        id: 589,
+    }, {
+        name: "test91",
+        hex: "#bccfdf",
+        id: 590,
+    }, {
+        name: "test92",
+        hex: "#96accb",
+        id: 591,
+    }, {
+        name: "test93",
+        hex: "#54de37",
+        id: 592,
+    }, {
+        name: "test94",
+        hex: "#d4ec65",
+        id: 593,
+    }, {
+        name: "test95",
+        hex: "#778b98",
+        id: 594,
+    }, {
+        name: "test96",
+        hex: "#576c7a",
+        id: 595,
+    }, {
+        name: "test97",
+        hex: "#868684",
+        id: 596,
     },
 ];
 
@@ -461,6 +852,103 @@ ALL_BRICKLINK_SOLID_COLORS.forEach((color) => {
 });
 
 const KNOWN_BRICKLINK_STUD_COLOR_NAMES = [
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+    "test6",
+    "test7",
+    "test8",
+    "test9",
+    "test10",
+    "test11",
+    "test12",
+    "test13",
+    "test14",
+    "test15",
+    "test16",
+    "test17",
+    "test18",
+    "test19",
+    "test20",
+    "test21",
+    "test22",
+    "test23",
+    "test24",
+    "test25",
+    "test26",
+    "test27",
+    "test28",
+    "test29",
+    "test30",
+    "test31",
+    "test32",
+    "test33",
+    "test34",
+    "test35",
+    "test36",
+    "test37",
+    "test38",
+    "test39",
+    "test40",
+    "test41",
+    "test42",
+    "test43",
+    "test44",
+    "test45",
+    "test46",
+    "test47",
+    "test48",
+    "test49",
+    "test50",
+    "test51",
+    "test52",
+    "test53",
+    "test54",
+    "test55",
+    "test56",
+    "test57",
+    "test58",
+    "test59",
+    "test60",
+    "test61",
+    "test62",
+    "test63",
+    "test64",
+    "test65",
+    "test66",
+    "test67",
+    "test68",
+    "test69",
+    "test70",
+    "test71",
+    "test72",
+    "test73",
+    "test74",
+    "test75",
+    "test76",
+    "test77",
+    "test78",
+    "test79",
+    "test80",
+    "test81",
+    "test82",
+    "test83",
+    "test84",
+    "test85",
+    "test86",
+    "test87",
+    "test88",
+    "test89",
+    "test90",
+    "test91",
+    "test92",
+    "test93",
+    "test94",
+    "test95",
+    "test96",
+    "test97",
     "Black",
     "Blue",
     "Bright Green",
@@ -524,6 +1012,103 @@ const KNOWN_BRICKLINK_STUD_COLOR_NAMES = [
 ];
 
 const KNOWN_BRICKLINK_TILE_COLOR_NAMES = [
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+    "test6",
+    "test7",
+    "test8",
+    "test9",
+    "test10",
+    "test11",
+    "test12",
+    "test13",
+    "test14",
+    "test15",
+    "test16",
+    "test17",
+    "test18",
+    "test19",
+    "test20",
+    "test21",
+    "test22",
+    "test23",
+    "test24",
+    "test25",
+    "test26",
+    "test27",
+    "test28",
+    "test29",
+    "test30",
+    "test31",
+    "test32",
+    "test33",
+    "test34",
+    "test35",
+    "test36",
+    "test37",
+    "test38",
+    "test39",
+    "test40",
+    "test41",
+    "test42",
+    "test43",
+    "test44",
+    "test45",
+    "test46",
+    "test47",
+    "test48",
+    "test49",
+    "test50",
+    "test51",
+    "test52",
+    "test53",
+    "test54",
+    "test55",
+    "test56",
+    "test57",
+    "test58",
+    "test59",
+    "test60",
+    "test61",
+    "test62",
+    "test63",
+    "test64",
+    "test65",
+    "test66",
+    "test67",
+    "test68",
+    "test69",
+    "test70",
+    "test71",
+    "test72",
+    "test73",
+    "test74",
+    "test75",
+    "test76",
+    "test77",
+    "test78",
+    "test79",
+    "test80",
+    "test81",
+    "test82",
+    "test83",
+    "test84",
+    "test85",
+    "test86",
+    "test87",
+    "test88",
+    "test89",
+    "test90",
+    "test91",
+    "test92",
+    "test93",
+    "test94",
+    "test95",
+    "test96",
+    "test97",
     "Black",
     "Blue",
     "Bright Light Blue",
@@ -594,8 +1179,8 @@ const BRICKLINK_TILE_COLORS = ALL_BRICKLINK_SOLID_COLORS.filter((color) =>
 ).sort((a, b) => {
     return a.name > b.name ? 1 : -1;
 });
-
-const ADDITIONAL_COLORS = ["Very Light Gray"];
+/**預設顏色：改成test92自定義顏色 */
+const ADDITIONAL_COLORS = ["test92"];//["Very Light Gray"];
 let ALL_VALID_BRICKLINK_COLORS = ALL_BRICKLINK_SOLID_COLORS.sort((a, b) => {
     return a.name > b.name ? 1 : -1;
 });
